17f6cd3d71fcd1a1ad14f57c77a3e9ee90c8c182
Marcio Cruz de Almeida <marciocadev@gmail.com>
2024-03-27 08:28:13 -0300

    Initial commit

---
+++ test/queue-batch-item-failures.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as QueueBatchItemFailures from '../lib/queue-batch-item-failures-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/queue-batch-item-failures-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new QueueBatchItemFailures.QueueBatchItemFailuresStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
